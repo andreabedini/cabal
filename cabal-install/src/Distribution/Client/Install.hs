f6e1547f6dd3923a1548847be33daa371292ac01
Andrea Bedini <andrea@andreabedini.com>
2025-03-19 13:31:48 +0800

    refactor(cabal-install): remove independent goals

--- cabal-install/src/Distribution/Client/Install.hs
+++ cabal-install/src/Distribution/Client/Install.hs
@@ -125,8 +125,8 @@ import qualified Distribution.Client.SolverInstallPlan as SolverInstallPlan
 import Distribution.Client.Tar (extractTarGzFile)
 import Distribution.Client.Targets
 import Distribution.Client.Types as Source
-import Distribution.Client.Types.ReadyPackage (ReadyPackage)
 import Distribution.Client.Types.OverwritePolicy (OverwritePolicy (..))
+import Distribution.Client.Types.ReadyPackage (ReadyPackage)
 import qualified Distribution.Client.Win32SelfUpgrade as Win32SelfUpgrade
 import qualified Distribution.InstalledPackageInfo as Installed
 import Distribution.Solver.Types.PackageFixedDeps
@@ -598,7 +598,6 @@ planPackages
               then Nothing
               else Just maxBackjumps
           )
-          . setIndependentGoals independentGoals
           . setReorderGoals reorderGoals
           . setCountConflicts countConflicts
           . setFineGrainedConflicts fineGrainedConflicts
@@ -668,7 +667,6 @@ planPackages
       countConflicts = fromFlag (installCountConflicts installFlags)
       fineGrainedConflicts = fromFlag (installFineGrainedConflicts installFlags)
       minimizeConflictSet = fromFlag (installMinimizeConflictSet installFlags)
-      independentGoals = fromFlag (installIndependentGoals installFlags)
       avoidReinstalls = fromFlag (installAvoidReinstalls installFlags)
       shadowPkgs = fromFlag (installShadowPkgs installFlags)
       strongFlags = fromFlag (installStrongFlags installFlags)
