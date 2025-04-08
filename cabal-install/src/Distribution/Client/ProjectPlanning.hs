b9a625a56010afce47a7f2be022920939dc85877
Andrea Bedini <andrea@andreabedini.com>
2025-04-08 16:04:17 +0800

    refactor(cabal-install): resolve package dbs during planning

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -2266,7 +2266,7 @@ elaborateInstallPlan
               if shouldBuildInplaceOnly pkg
                 then BuildInplaceOnly OnDisk
                 else BuildAndInstall
-            elabPackageDbs = projectConfigPackageDBs sharedPackageConfig
+            elabPackageDbs = Cabal.interpretPackageDbFlags False (projectConfigPackageDBs sharedPackageConfig)
             elabBuildPackageDBStack = buildAndRegisterDbs
             elabRegisterPackageDBStack = buildAndRegisterDbs
 
