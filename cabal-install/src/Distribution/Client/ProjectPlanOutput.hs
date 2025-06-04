d27ca1d6cc04bc9072ef42ce6e9ba8ecd8aec00f
Andrea Bedini <andrea@andreabedini.com>
2025-06-04 20:04:48 +0800

    feat: add stage information to JSON output in encodePlanAsJson

--- cabal-install/src/Distribution/Client/ProjectPlanOutput.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanOutput.hs
@@ -166,6 +166,7 @@ encodePlanAsJson distDirLayout elaboratedInstallPlan elaboratedSharedConfig =
                   then "installed"
                   else "configured"
               )
+        , "stage" J..= jdisplay (elabStage elab)
         , "id" J..= (jdisplay . Graph.nodeKey) elab
         , "pkg-name" J..= (jdisplay . pkgName . packageId) elab
         , "pkg-version" J..= (jdisplay . pkgVersion . packageId) elab
