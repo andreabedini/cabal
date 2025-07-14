4b4b19b56d01991896510fa67287f5a716a850cb
Andrea Bedini <andrea@andreabedini.com>
2025-07-14 12:42:15 +0800

    refactor(cabal-install): reduce scope in ProjectPlanning

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -1805,39 +1805,25 @@ elaborateInstallPlan
                     , elabComponentId = notImpl "elabComponentId"
                     , elabInstallDirs = notImpl "elabInstallDirs"
                     , elabPkgOrComp = ElabComponent (ElaboratedComponent
-                        { compSolverName
-                        , compComponentName
-                        , compLibDependencies
-                        , compLinkedLibDependencies
-                        , compExeDependencies
-                        , compPkgConfigDependencies
-                        , compExeDependencyPaths
-                        , compOrderLibDependencies
-                        , compInstantiatedWith
-                        , compLinkedInstantiatedWith
+                        { compSolverName = CD.ComponentSetup
+                        , compComponentName = Nothing
+                        , compLibDependencies =
+                            [ (configuredId cid, False)
+                            | cid <- CD.setupDeps solverPkgLibDeps >>= elaborateLibSolverId mapDep
+                            ]
+                        , compLinkedLibDependencies = notImpl "compLinkedLibDependencies"
+                        , compOrderLibDependencies = notImpl "compOrderLibDependencies"
+                        -- Not supported:
+                        , compExeDependencies = mempty
+                        , compExeDependencyPaths = mempty
+                        , compPkgConfigDependencies = mempty
+                        , compInstantiatedWith = mempty
+                        , compLinkedInstantiatedWith = Map.empty
                         })
                     }
             | otherwise =
                 Nothing
             where
-              compSolverName = CD.ComponentSetup
-              compComponentName = Nothing
-
-              dep_pkgs = elaborateLibSolverId mapDep =<< CD.setupDeps solverPkgLibDeps
-
-              compLibDependencies =
-                -- MP: No idea what this function does
-                map (\cid -> (configuredId cid, False)) dep_pkgs
-              compLinkedLibDependencies = notImpl "compLinkedLibDependencies"
-              compOrderLibDependencies = notImpl "compOrderLibDependencies"
-
-              -- Not supported:
-              compExeDependencies = []
-              compExeDependencyPaths = []
-              compPkgConfigDependencies = []  
-              compInstantiatedWith = mempty
-              compLinkedInstantiatedWith = Map.empty
-
               notImpl f =
                 error $
                   "Distribution.Client.ProjectPlanning.setupComponent: "
@@ -1938,13 +1924,14 @@ elaborateInstallPlan
                       { compSolverName
                       , compComponentName
                       , compLibDependencies
-                      , compLinkedLibDependencies
                       , compExeDependencies
                       , compPkgConfigDependencies
                       , compExeDependencyPaths
-                      , compOrderLibDependencies
                       , compInstantiatedWith = Map.empty
                       , compLinkedInstantiatedWith = Map.empty
+                      -- filled later (in step 5)
+                      , compLinkedLibDependencies = error "buildComponent: compLinkedLibDependencies"
+                      , compOrderLibDependencies = error "buildComponent: compOrderLibDependencies"
                       }
 
                 -- 3. Construct a preliminary ElaboratedConfiguredPackage,
@@ -2036,9 +2023,6 @@ elaborateInstallPlan
 
                 return ((cc_map', lc_map', exe_map'), elab)
             where
-              compLinkedLibDependencies = error "buildComponent: compLinkedLibDependencies"
-              compOrderLibDependencies = error "buildComponent: compOrderLibDependencies"
-
               cname = Cabal.componentName comp
               compComponentName = Just cname
               compSolverName = CD.componentNameToComponent cname
